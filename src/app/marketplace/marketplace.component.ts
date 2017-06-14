309fd3f5a7285d32843d93fe02abf16b19916b6f
Ben Petrosky and Elan Baharie <ben+elan@>
2017-06-14 11:26:30 -0700

    add update functionality with link to firebase

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Album } from '../album.model';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-marketplace',
@@ -10,16 +12,18 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
-  albums: Album[];
+  albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private albumService: AlbumService){}
 
-  goToDetailPage(clickedAlbum: Album) {
-    this.router.navigate(['albums', clickedAlbum.id]);
+  goToDetailPage(clickedAlbum) {
+    this.router.navigate(['albums', clickedAlbum.$key])
   };
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
+    console.log(this.router.url);
   }
 
 }
